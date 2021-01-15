add4a3ee586727165714e3afddd50e5cf1eacc48
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2021-01-15 07:55:48 -0500

    cosmetic changes

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../services/pokemon.service';
 import { Pokemon, PokedexPokemon } from '../models/pokemon.model';
+import { PokedexModalComponent } from './pokedex-modal/pokedex-modal.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'pokedex',
@@ -13,7 +15,9 @@ export class PokedexComponent implements OnInit {
   pokemon: Pokemon;
   genOnePokemon: PokedexPokemon[];
 
-  constructor(private pokemoneService: PokemonService) { }
+  constructor(
+    private pokemoneService: PokemonService,
+    private modalService: NgbModal) { }
 
   ngOnInit() {
     // todo
@@ -23,7 +27,8 @@ export class PokedexComponent implements OnInit {
     this.pokemoneService.getPokemon(this.searchPokemon).subscribe(pokemon =>{
       this.pokemon = pokemon;
       this.searchPokemon = '';
-      console.log(pokemon);
+      const modalRef = this.modalService.open(PokedexModalComponent);
+      modalRef.componentInstance.dexNumber = pokemon.id;
     })
   }
 }
