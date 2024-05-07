4a3a8df842380f66d64351b2e3ad1e572600cc0a
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2024-05-06 20:51:35 -0400

    update to 15

--- src/app/constants/kanto-event.constants.ts
+++ src/app/constants/kanto-event.constants.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({ providedIn: 'root'})
+@Injectable()
 export class KantoEventConstants {
     pokemonIndexOfNeeded: number[] = [17, 18, 21, 22, 28, 35, 36, 37, 38, 42, 46, 47,
         48, 49, 51, 52, 53, 70, 71, 73, 79, 80, 81, 82, 83, 84, 85, 89, 103, 106, 107,
